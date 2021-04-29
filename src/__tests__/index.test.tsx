1930edd206f203ba691170db2f9431312d70ab91
Remco Pander <rpander93@gmail.com>
2021-04-29 20:34:13 +0200

    test: Add unit test for JS code

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,45 @@
-it.todo('write a test');
+import { NativeModules, Platform } from 'react-native';
+import * as DocumentOpener from '../index';
+
+jest.mock('react-native', () => ({
+  NativeModules: {
+    DocumentOpener: {
+      openAsync: jest.fn(() => Promise.resolve()),
+      presentOptionsAsync: jest.fn(() => Promise.resolve()),
+    },
+  },
+  Platform: {
+    OS: 'ios',
+  },
+}));
+
+describe('DocumentOpener', () => {
+  it('opens a document using the native module', () => {
+    const filePath = 'file:///data/0/emulated/com.example.reactnativedocumentopener/files/example.mp4';
+    const mimeType = 'video/mp4';
+
+    DocumentOpener.openAsync(filePath, mimeType);
+
+    const spy = jest.spyOn(NativeModules.DocumentOpener, 'openAsync');
+    expect(spy).toHaveBeenCalledWith(filePath.substr(7), mimeType);
+  });
+
+  it('opens document options using the native module', () => {
+    const filePath = 'file:///data/0/emulated/com.example.reactnativedocumentopener/files/example.mp4';
+    const mimeType = 'video/mp4';
+
+    DocumentOpener.presentOptionsAsync(filePath, mimeType);
+
+    const spy = jest.spyOn(NativeModules.DocumentOpener, 'presentOptionsAsync');
+    expect(spy).toHaveBeenCalledWith(filePath.substr(7), mimeType);
+  });
+
+  it('throws error when trying to present document options on Android', () => {
+    const t = () => {
+      Platform.OS = 'android';
+      DocumentOpener.presentOptionsAsync('file://does-not-exist.mp4');
+    };
+
+    expect(t).toThrowError();
+  });
+});

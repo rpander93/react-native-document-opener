18852f595c4771fafb2e701f1b93306b69a18307
Remco Pander <rpander93@gmail.com>
2021-05-26 17:24:58 +0200

    refactor: remove usage of `documentController` var since it's unnecessary

--- ios/DocumentOpener.swift
+++ ios/DocumentOpener.swift
@@ -2,45 +2,39 @@ import UIKit
 
 @objc(DocumentOpener)
 class DocumentOpener: NSObject, UIDocumentInteractionControllerDelegate {
-    var documentController: UIDocumentInteractionController?
-    
     @objc(openAsync:mimeType:resolve:reject:)
     func openAsync(_ filePath: String, mimeType: String?, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
-        documentController = UIDocumentInteractionController(url: URL.init(fileURLWithPath: filePath))
-        documentController?.delegate = self
-        
-        DispatchQueue.main.async { [weak self] in
-            if self?.documentController?.presentPreview(animated: true) == true {
+        let documentController = UIDocumentInteractionController(url: URL.init(fileURLWithPath: filePath))
+        documentController.delegate = self
+
+        DispatchQueue.main.async {
+            if documentController.presentPreview(animated: true) {
                 resolve(nil)
             } else {
                 reject("EINVAL", "Document is not supported", nil)
             }
         }
     }
-    
+
     @objc(presentOptionsAsync:resolve:reject:)
     func presentOptionsAsync(_ filePath: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
-        documentController = UIDocumentInteractionController(url: URL.init(fileURLWithPath: filePath))
-        documentController?.delegate = self
-                
-        DispatchQueue.main.async { [weak self] in
+        let documentController = UIDocumentInteractionController(url: URL.init(fileURLWithPath: filePath))
+        documentController.delegate = self
+
+        DispatchQueue.main.async {
             let rectangle = CGRect(x: 0, y: 0, width: 0, height: 0)
             let root = UIApplication.shared.delegate!.window!!.rootViewController!
 
-            if self?.documentController?.presentOptionsMenu(from: rectangle, in: root.view, animated: true) == true {
+            if documentController.presentOptionsMenu(from: rectangle, in: root.view, animated: true) {
                 resolve(nil)
             } else {
                 reject("EINVAL", "Document is not supported", nil)
             }
         }
     }
-    
+
     // MARK: -UIDocumentInteractionControllerDelegate
     func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
         return RCTPresentedViewController()!
-    }
-    
-    func documentInteractionControllerDidEndPreview(_ controller: UIDocumentInteractionController) {
-        self.documentController = nil
     }
 }
